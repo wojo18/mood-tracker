1732d52347d06e36cc05721aed8705a977341b69
wojo18 <wojti_ww@interia.pl>
2025-08-03 17:44:35 +0200

    Create about.js

---
+++ app/about.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function About() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>To jest ekran "O mnie"</Text>
+    </View>
+  );
+}
